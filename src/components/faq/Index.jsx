d05240d455a88371ee3d0efd8b1ed1bb3cd94bc0
basudevkumar <jhulonkumar40@gmail.com>
2026-04-02 10:14:56 +0600

    fix: in FaqComp remove background color from main section

--- src/components/faq/Index.jsx
+++ src/components/faq/Index.jsx
@@ -38,7 +38,7 @@ const FaqComp = () => {
   };
 
   return (
-    <section className="py-10 sm:py-14 md:py-16 lg:py-20 bg-gradient-to-br from-[#f8fffe] to-[#e8f5ef]">
+    <section className="py-10 sm:py-14 md:py-16 lg:py-20 ">
       <Container>
         <div className="px-4 sm:px-6 md:px-0 max-w-3xl mx-auto">
           
